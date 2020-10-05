2fa7881bf383eac5ad720d3cbd71de9f23b62d8d
Stanislaw <bzdety.o.poranku@ugmail.com>
2020-10-05 13:13:44 +0200

    Last goodbye with shity sequelize

--- commands/profile.js
+++ commands/profile.js
@@ -3,15 +3,11 @@ module.exports = {
 	description: 'me',
 	execute(message, args) {
 	const Discord = require('discord.js');	
-	const sequelize = require('sequelize')
 	let Id = message.author.id;
 	let waifucount = 0;
 	let coins = 3;
 	let tickets = 4;
 	let malname = '-';
-	let USER = user.findOrCreate({where: {IDD: Id}, defaults: {coins: 1, tickets:1, malname: 'Not set'}});
-		coins = USER.getDataValue('IDD');
-		console.log(USER.IDD);
 	let embed = new Discord.MessageEmbed()
 		.setColor("#ff5e54")
 		.setTitle(message.author.username)
